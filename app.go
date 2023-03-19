f92324ed7d1fa2a53c8b4ccbafa49dce88f94952
B3ryll <b3ryll@proton.me>
2023-03-19 19:21:45 -0300

    v0.1.0+20230319.01: Initial Commit
    + Write app.go file
    + Add gorilla/websocket as dependency for handling websocket connections
    
    + Write index.html file
    + Write app.js file for frontend user interface

---
+++ app.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "fmt"
+    . "net/http"
+    _ "strings"
+
+    "github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+    indexHandle := func (writ ResponseWriter, req *Request) {
+        ServeFile(writ, req, "index.html")
+    }
+    HandleFunc("/", indexHandle)
+
+    jsAppHandle := func (writ ResponseWriter, req *Request) {
+        ServeFile(writ, req, "app.js")
+    }
+    HandleFunc("/app.js", jsAppHandle)
+
+    chatHandle := func (writ ResponseWriter, req *Request) {
+        conn, err := upgrader.Upgrade(writ, req, nil)
+        if err != nil {
+            fmt.Print("upgrade failed: ", err)
+            return
+        }
+        defer conn.Close()
+
+        for {
+            mt, msg, err := conn.ReadMessage()
+            if err != nil {
+                fmt.Println("read failed: ", err)
+                break
+            }
+
+            input   := string(msg)
+            message := input + "(check)"
+
+            output := []byte(message)
+
+            err = conn.WriteMessage(mt, output)
+            if err != nil {
+                fmt.Println("write failed:", err)
+                break
+            }
+        }
+    }
+    HandleFunc("/chat", chatHandle)
+
+    fmt.Printf("running webserver on port 8080\n")
+    ListenAndServe(":8080", nil)
+}
